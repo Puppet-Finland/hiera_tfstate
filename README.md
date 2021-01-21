96b689dcaf18360f00fe230ed80df5ea1b6d2968
Samuli Seppänen <samuli.seppanen@puppeteers.fi>
2021-01-21 17:01:11 +0200

    Initial import
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.fi>

---
+++ README.md
@@ -0,0 +1,110 @@
+# hiera_tfstate
+
+Hiera backend for reading data from Terraform state files. Currently Terraform
+0.13.x state files are supported.
+
+**WARNING**: right now this Hiera backend is still highly experimental. Use it
+at your own risk.
+
+# How does it work?
+
+Terraform state files are JSON so they're technically compatible with the
+default JSON Hiera backend. However, all the resources in the state file are in
+one huge array and referring to them would require using the resource index.
+As the index can change raw state files are rather useless for Hiera.
+
+This Hiera backend takes the Terraform state file and produces a flattened list
+of data and resource attribute paths and their values. All values are prefixed
+with "tfstate". Some examples:
+
+    tfstate::aws_acm_certificate::my-alb::arn: <arn>
+    tfstate::module::my_instance::aws_instance::ec2_instance::0::private_ip: 10.9.2.50
+
+If and only if all resources/data sources are stored inside a module you can
+use *no_root_module: true* in the options hash to remove the root module away
+from the paths.
+
+# Setup
+
+Setup should be straightforward:
+
+* Add this Puppet module to your Puppetfile (or otherwise install it)
+* Configure this backend in hiera.yaml (see below)
+* Install Ruby dependencies, if any (see below)
+* Backend-specific configuration, if any (see below)
+
+# Supported state file backends
+
+## file
+
+The *file* backend loads the state file from a local file. This has the best
+performance, but requires having a copy of the state file locally. You also
+need to take care of updating the state file periodically or on-demand when it
+changes.
+
+Example for hiera.yaml:
+
+    - name: "Terraform state file"
+      data_hash: hiera_tfstate
+      options:
+        backend: 'file'
+        statefile: '/var/lib/misc/.tfstate'
+
+## s3
+
+This backend loads the state file from Amazon S3 bucket. You will need to
+install the AWS S3 SDK for Ruby to use it:
+
+    $ /opt/puppetlabs/puppet/bin/gem install aws-sdk-s3
+
+Example for hiera.yaml:
+
+    - name: "Terraform state file"
+      data_hash: hiera_tfstate
+      options:
+        backend: 's3'
+        profile: 'hiera'
+        bucket: 'terraform-state.example.org'
+        key: 'foobar'
+
+Credentials and region can be fetched from the profile (/root/.aws/credentials):
+
+    [hiera]
+    aws_access_key_id = <access-key-id>
+    aws_secret_access_key = <secret-access-key>
+    region = us-west-1
+
+If the profile is missing then usual environment variables will be used:
+
+    * AWS_ACCESS_KEY_ID
+    * AWS_SECRET_ACCESS_KEY
+    * AWS_REGION
+
+# Common configuration options
+
+The "options" has support the following common options:
+
+* *no_root_module*: remove the root module name from all resource paths. This only works if *all* resources in your state file are inside a module. Valid values are *true* and *false*.
+* *debug*: print debugging information such as the Hiera-compatible state file
+
+# Testing lookups
+
+It is recommended to test lookups in a feature environment before trying to actually use this backend in Puppet code:
+
+    $ puppet lookup tfstate::aws_acm_certificate::my-alb::arn --node puppet.example.org --environment my_feature
+
+To view debugging information add *--explain*:
+
+    $ puppet lookup tfstate::aws_acm_certificate::my-alb::arn --node puppet.example.org --environment my_feature --explain
+
+To view the produced data hash in yaml format set *debug: true* in the
+hiera.yaml options hash for this backend, then run the command above.
+
+# Safety precautions
+
+You **should** use proper data types in your Hiera lookups to ensure that the data
+you get matches your expectations.
+
+# License
+
+This software is licensed under the [BSD-2-Clause license](LICENSE).
---
+++ lib/puppet/functions/hiera_tfstate.rb
@@ -0,0 +1,123 @@
+Puppet::Functions.create_function(:hiera_tfstate) do
+  require 'json'
+  require 'yaml'
+
+  dispatch :data_hash do
+    param 'Hash', :options
+    param 'Puppet::LookupContext', :context
+  end
+
+  def data_hash(options, context)
+    context.explain { "Backend options: #{options}" }
+    raw_state = load_tfstate(options)
+    raw_state = JSON.parse(raw_state)
+
+    # Ensure that the state file belongs to a known good Terraform version
+    validate_terraform_version(raw_state)
+
+    # Ensure that root module can be removed from the resource paths without
+    # causing unexpected behavior. Essentially require that all resources must
+    # be defined inside a module when that option is enabled.
+    validate_resource_paths(raw_state, options)
+
+    # Convert the resources in the state into a format that can actually be
+    # digested by Hiera lookups. The main problem with raw state files is
+    # that all the resources are inside an array, so one would have to use
+    # potentially volative indices to refer to the resources and their
+    # attributes.
+    state = convert_state(raw_state, options, context)
+    state
+  end
+
+  def load_tfstate(options)
+    if options['backend'] == 'file'
+      load_file(options)
+    elsif options['backend'] == 's3'
+      download_from_s3(options)
+    else
+      raise Puppet::DataBinding::LookupError, "[hiera_tfstate] Unsupported backend: #{options['backend']}"
+    end
+  end
+
+  def load_file(options)
+    begin
+      state_content = File.read(options['statefile'])
+    rescue Errno::ENOENT
+      raise Puppet::DataBinding::LookupError "ERROR: terraform state file #{options['statefile']} not found!"
+    end
+    state_content
+  end
+
+  def download_from_s3(options)
+    require 'aws-sdk-s3'
+    s3_client = Aws::S3::Client.new(profile: options['profile'])
+    resp = s3_client.get_object(bucket: options['bucket'], key: options['key'])
+    resp.body.read
+  end
+
+  def validate_terraform_version(raw_state)
+    return unless raw_state['terraform_version'] =~ /0\.13\.\d+/
+
+    raise 'ERROR: this Hiera backend supports only Terraform 0.13.x state files.'
+  end
+
+  def validate_resource_paths(raw_state, options)
+    return unless options['no_root_module']
+
+    raw_state['resources'].each do |resource|
+      unless resource['module']
+        raise "Error: remove_root_module_from_path works only if all resources are in a module!')"
+      end
+    end
+  end
+
+  def get_module_path(mod, options)
+    if options['no_root_module']
+      mod.split('.').drop(2)
+    else
+      mod.split('.')
+    end
+  end
+
+  def convert_state(raw_state, options, context)
+    resources = {}
+    raw_state['resources'].each do |resource|
+      # Construct a Hiera-style path to the Terraform resource
+      # and its attributes
+      resource_path = []
+
+      # Examples of contents of resource['module']:
+      #
+      # - nil (resource defined in the root module)
+      # - module.somemodule
+      # - module.somemodule.module.nestedmodule
+      #
+      mod = if resource['module'].nil?
+              ''
+            else
+              resource['module']
+            end
+
+      module_path = get_module_path(mod, options)
+
+      resource_path.append('tfstate')
+      resource_path.append(module_path) unless module_path.empty?
+      resource_path.append(resource['type'])
+      resource_path.append(resource['name'])
+
+      resource['instances'].each do |instance|
+        resource_path.append(instance['index_key'])
+
+        instance['attributes'].each do |attribute|
+          resource_path.push(attribute[0])
+          flattened = resource_path.flatten.join('::')
+          resources[flattened] = attribute[1]
+          resource_path.pop
+        end
+      end
+    end
+
+    context.explain { YAML.dump(resources) } if options['debug']
+    resources
+  end
+end
